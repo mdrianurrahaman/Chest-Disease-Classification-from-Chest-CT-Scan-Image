611240a5c8ece9d5e6384e57e8f5e932f8b2370f
rian <mdrianurrahaman@gmail.com>
2024-03-18 12:02:51 +0530

    updated files

--- src/cnnClassifier/components/model_evaluation.py
+++ src/cnnClassifier/components/model_evaluation.py
@@ -55,7 +55,7 @@ class Evaluation:
     
     def log_into_mlflow(self):
         mlflow.set_registry_uri(self.config.mlflow_uri)
-        tracking_url_type_store = urlparse(mlflow.get_tracking_uri("")).scheme
+        tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
         
         with mlflow.start_run():
             mlflow.log_params(self.config.all_params)
